8bfb62f4b7bf0cfff0ea78baa54832a5d58127a5
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2025-04-29 19:50:27 -0300

    Adding logout route

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
