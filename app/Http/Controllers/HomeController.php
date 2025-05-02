ee075f486d94d70001fda3d99493d057b77cabf5
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2025-05-02 11:25:11 -0300

    Modify home links and take products for DB

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Platform;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $platforms = Platform::all();
-        return view('home', compact('platforms'));
+        $products = Product::with(['gender', 'platform'])->take(4)->get();
+        return view('home', compact('platforms', 'products'));
     }
 }
